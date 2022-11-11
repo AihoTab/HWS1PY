e27cf62becd5a4429965263b3315cf04447b8d82
Jull <jullweas@gmail.com>
2022-11-11 06:57:40 +0300

    task4

--- HW1.py
+++ HW1.py
@@ -1,14 +1,15 @@
-# 3 Напишите программу, которая принимает на вход координаты точки (X и Y),
-# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
-x = int(input())
-y = int(input())
-if x > 0 and y > 0:
-    print(1)
-elif x < 0 and y > 0:
-    print(2)
-elif x < 0 and y < 0:
-    print(3)
-elif x < 0 and y < 0:
-    print(4)
-elif x == 0 or y == 0:
-    print('wrong')
+# 4 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+a = int(input())
+p_inf = float('inf')
+n_inf = float('-inf')
+if a == 1:
+    print(f'Х {0, p_inf} Y {0, p_inf}')
+elif a == 2:
+    print(f'Х {0, n_inf} Y {0, p_inf}')
+elif a == 3:
+    print(f'Х {0, n_inf} Y {0, n_inf}')
+elif a == 4:
+    print(f'Х от {0} до {p_inf} Y {0, n_inf}')
+else:
+    print('Введено неверное значение')
